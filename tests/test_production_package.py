610920caf8268dae5eaa909268a6f4c62cce8bc4
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:38 +0200

    Update test to version 4.0

--- tests/test_production_package.py
+++ tests/test_production_package.py
@@ -4,30 +4,22 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class ProductionPackageTestCase(unittest.TestCase):
-    'Test production_package module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('production_package')
-
-    def test0005views(self):
-        'Test views'
-        test_view('production_package')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class TestCase(ModuleTestCase):
+    'Test module'
+    module = 'production_package'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            ProductionPackageTestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_production_package.rst',
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_production_package.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
